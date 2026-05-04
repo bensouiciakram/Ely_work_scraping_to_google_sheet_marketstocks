cf44681f2a1a044d75e33d87fde9962082dd8d37
bensouici akram <bensouiciakram@gmail.com>
2026-05-04 13:33:11 +0100

    fixing the parse and blocking in gurus.py

--- web_site/gurus.py
+++ web_site/gurus.py
@@ -6,6 +6,9 @@ import time
 from playwright.sync_api import sync_playwright
 from helpers.params import HEADERS
 
+from parsel import Selector 
+from playwright_stealth import Stealth
+
 class Gurus:
     @staticmethod
     def scrape_dcf_pct(stock_symbol, retries=2, delay=5):
@@ -13,29 +16,30 @@ class Gurus:
         url = GURUS_BASE_URL.format(stock_symbol)
         for i in range(retries):
             try:
-                with sync_playwright() as p:
-                    browser = p.chromium.launch(headless=True)
+                # with sync_playwright() as p:
+                with Stealth().use_sync(sync_playwright()) as p : 
+                    browser = p.chromium.launch(headless=False)
                     context = browser.new_context(user_agent=HEADERS["User-Agent"])
                     page = context.new_page()
                     page.goto(url)
                     page.wait_for_timeout(5000)
-                    content = page.content()
+                    page.wait_for_selector('//div[contains(@class,"dcf-table-row")]')
+                    selector = Selector(text=page.content())
 
-                    # Parse the content with BeautifulSoup
-                    soup = BeautifulSoup(content, "html.parser")
                     stock_details = []
 
                     # Extract Stock Price
-                    stock_price = soup.select(".el-col.inline.p-l-xs .stock-dcf-table .text-right.fs-regular.el-col.el-col-8")
-                    stock_price = stock_price[0].get_text(strip=True) if len(stock_price) >= 1 else ""
-                    
+                    stock_price = selector.xpath('string(//div[@class="summary capture-area"]/span)').re_first(r'\d+\.\d+')
+
                     # Extract Fair Value
-                    fair_value = soup.select(".el-col.inline.p-l-xs .stock-dcf-table .fs-x-large.fw-bolder.text-right.el-col.el-col-12")
-                    fair_value = fair_value[0].get_text(strip=True) if len(fair_value) >= 1 else ""
+                    fair_value = selector.xpath(
+                        'string(//div[contains(text(),"Fair Value")]/ancestor::div[1]/following-sibling::div)'
+                        ).get('').strip()
                     
                     # Extract Margin of Safety
-                    margin_of_safety = soup.select(".el-col.inline.p-l-xs .stock-dcf-table .dcf-result")
-                    margin_of_safety = margin_of_safety[0].get_text(strip=True) if len(margin_of_safety) >= 1 else ""
+                    margin_of_safety = selector.xpath(
+                        '//div[contains(text(),"Margin of Safety")]/ancestor::div[1]/following-sibling::div//i/following-sibling::text()'
+                        ).get('').strip()
                     
                     # Clean up and store the extracted details
                     stock_details.extend([SheetManipulator.clean_non_ascii(fair_value), SheetManipulator.clean_non_ascii(margin_of_safety)])
